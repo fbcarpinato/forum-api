d2e0ae8cc148046baa6aa26bcd5b29fff47a7d00
Fabio Carpinato <fabiocarpinato@outlook.it>
2017-12-04 17:29:07 +0100

    adds circle ci integration

--- app/Http/Controllers/v1/UsersController.php
+++ app/Http/Controllers/v1/UsersController.php
@@ -6,14 +6,29 @@ use Forum\Http\Controllers\ApiController;
 use Forum\Http\Resources\UserResource;
 use Forum\Models\User;
 
+/**
+ * @resource Users
+ * @package Forum\Http\Controllers\v1
+ */
 class UsersController extends ApiController
 {
+    /**
+     * Fetch all the users.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         $users = User::all();
         return $this->respondSuccess(UserResource::collection($users));
     }
 
+    /**
+     * Fetch a single user.
+     *
+     * @param User $user
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show(User $user)
     {
         return $this->respondSuccess(new UserResource($user));
